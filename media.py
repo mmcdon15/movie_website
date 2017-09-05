0f20452af91299b4f7e6f3604503e11436256e0d
Michael McDonald <mmcdon15@gmail.com>
2017-09-05 09:03:30 -0400

    fixed stylistic issues

--- media.py
+++ media.py
@@ -1,13 +1,12 @@
 import webbrowser
-#code based off of the website mini-project from the Udacity nanodegree
+# code based off of the website mini-project from the Udacity nanodegree
 
 
 class Movie():
-        def __init__(self, movie_title, movie_storyline, poster_image,
+        def __init__(self, movie_title, poster_image,
                      trailer_youtube):
             # Constructor
             self.title = movie_title
-            self.storyline = movie_storyline
             self.poster_image_url = poster_image
             self.trailer_youtube_url = trailer_youtube
 
